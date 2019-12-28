ada0292aeeb45126ae6ed22a33627fe76b26183f
michael eo <michael@michaels-MacBook-Pro.local>
2019-12-28 17:39:22 +1300

    3

---
+++ sins/Pride.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, Image,  } from 'react-native';
+
+
+
+export default class Maps extends React.Component {
+
+    render() {
+        return (
+              <View>
+                <Text>I. 교만 (Pride) </Text>
+
+              </View> 
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    }
+})
+
