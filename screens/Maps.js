216d54120b68cfb133633dc8b43292308053bce0
michael eo <michael@michaels-MacBook-Pro.local>
2019-12-28 16:44:15 +1300

    second

--- screens/Maps.js
+++ screens/Maps.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, Image, Dimensions } from 'react-native';
+import {StyleSheet, Text, Image, Dimensions} from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
 
 
@@ -7,13 +7,13 @@ export default class Maps extends React.Component {
 
     render() {
         return (
-            <ImageZoom cropWidth={Dimensions.get('window').width}
-                cropHeight={Dimensions.get('window').height}
-                imageWidth={Dimensions.get('window').width}
-                imageHeight={Dimensions.get('window').height}>
-                <Image style={style.rotation}
-                    source={require('./../Map/map1.png')} />
-            </ImageZoom>
+                <ImageZoom cropWidth={Dimensions.get('window').width}
+                    cropHeight={Dimensions.get('window').height}
+                    imageWidth={Dimensions.get('window').width}
+                    imageHeight={Dimensions.get('window').height}>
+                    <Image style={style.rotation}
+                        source={require('./../Map/map1.png')} />
+                </ImageZoom>
         )
     }
 }
